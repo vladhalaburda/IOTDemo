04473404a96173fee4eaa6ebc8d5c406c7b314aa
Vlad Halaburda <vladhalaburdade@gmail.com>
2024-12-02 23:59:56 +0100

    Init commit

---
+++ src/main/java/com/vladhalaburda/iotdemo/IotdemoApplication.java
@@ -0,0 +1,13 @@
+package com.vladhalaburda.iotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotdemoApplication.class, args);
+	}
+
+}
