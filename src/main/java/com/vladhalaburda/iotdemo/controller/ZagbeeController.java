0471f57706497c44cf1e50de400aecb1b937fc47
Vlad Halaburda <vladhalaburdade@gmail.com>
2024-12-04 03:44:37 +0100

    Fix bug with controller

--- src/main/java/com/vladhalaburda/iotdemo/controller/ZagbeeController.java
+++ src/main/java/com/vladhalaburda/iotdemo/controller/ZagbeeController.java
@@ -2,11 +2,11 @@ package com.vladhalaburda.iotdemo.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vladhalaburda.iotdemo.model.ZigbeeDevice;
@@ -15,13 +15,16 @@ import com.vladhalaburda.iotdemo.service.zigbee.ZigbeeMqttEmulator;
 @RestController
 @RequestMapping("/api/zagbee")
 public class ZagbeeController {
-    @Autowired
-    private ZigbeeMqttEmulator zigbeeMqttEmulator;
+    
+    private final ZigbeeMqttEmulator zigbeeMqttEmulator;
+    public ZagbeeController(ZigbeeMqttEmulator zigbeeMqttEmulator) {
+        this.zigbeeMqttEmulator = zigbeeMqttEmulator;
+    }
 
     @PostMapping("/add_device")
-    public String addDevice(@RequestParam ZigbeeDevice device) {
+    public ResponseEntity<String> addDevice(@RequestBody ZigbeeDevice device) {
         zigbeeMqttEmulator.addDevice(device);
-        return "OK";
+        return ResponseEntity.ok("Sensor added: " + device.getName());
     }
 
     @GetMapping("/list_device")
