cc2115a44fef9d71220686d286dd3c3649f5edc7
Vlad Halaburda <vladhalaburdade@gmail.com>
2024-12-04 03:22:37 +0100

    Add ZagbeeMqtt emulator

---
+++ src/main/java/com/vladhalaburda/iotdemo/model/ZigbeeDevice.java
@@ -0,0 +1,23 @@
+package com.vladhalaburda.iotdemo.model;
+
+import java.util.Map;
+
+public class ZigbeeDevice {
+    private String id;
+    private String name;
+    private String type;
+    private boolean state;
+    private Map<String, Object> parameters;
+
+    // Getters and Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public boolean isState() { return state; }
+    public void setState(boolean state) { this.state = state; }
+    public Map<String, Object> getParameters() { return parameters; }
+    public void setParameters(Map<String, Object> parameters) { this.parameters = parameters; }
+}
---
+++ src/main/java/com/vladhalaburda/iotdemo/service/zigbee/ZigbeeMqttEmulator.java
@@ -0,0 +1,98 @@
+package com.vladhalaburda.iotdemo.service.zigbee;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import com.vladhalaburda.iotdemo.model.ZigbeeDevice;
+
+public class ZigbeeMqttEmulator {
+    private static final String BROKER = "tcp://localhost:1883";
+    private static final String BASE_TOPIC = "zigbee2mqtt/";
+    private final Map<String, ZigbeeDevice> devices = new HashMap<>();
+    private final Random random = new Random();
+    private MqttClient mqttClient;
+
+    public ZigbeeMqttEmulator() throws MqttException {
+        mqttClient = new MqttClient(BROKER, MqttClient.generateClientId());
+        mqttClient.connect();
+        mqttClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.err.println("Connection lost: " + cause.getMessage());
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                handleCommand(topic, new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                // No-op
+            }
+        });
+        mqttClient.subscribe(BASE_TOPIC + "+/set");
+    }
+
+    public void addDevice(ZigbeeDevice device) {
+        devices.put(device.getId(), device);
+    }
+
+    public void removeDevice(String deviceId) {
+        devices.remove(deviceId);
+    }
+
+    public void startSimulation() {
+        new Thread(() -> {
+            while (true) {
+                for (ZigbeeDevice device : devices.values()) {
+                    publishData(device);
+                }
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }).start();
+    }
+
+    private void publishData(ZigbeeDevice device) {
+        Map<String, Object> data = new HashMap<>(device.getParameters());
+        data.put("state", device.isState());
+        if (device.getType().equals("temperature")) {
+            data.put("temperature", 20 + random.nextDouble() * 10);
+        } else if (device.getType().equals("humidity")) {
+            data.put("humidity", 30 + random.nextDouble() * 20);
+        }
+        try {
+            mqttClient.publish(BASE_TOPIC + device.getName(), new MqttMessage(data.toString().getBytes()));
+            System.out.println("Published data for " + device.getName() + ": " + data);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleCommand(String topic, String payload) {
+        String deviceId = topic.split("/")[1];
+        ZigbeeDevice device = devices.get(deviceId);
+        if (device == null) {
+            System.err.println("Device not found: " + deviceId);
+            return;
+        }
+        if (payload.contains("\"state\":\"ON\"")) {
+            device.setState(true);
+        } else if (payload.contains("\"state\":\"OFF\"")) {
+            device.setState(false);
+        }
+        System.out.println("Updated state for " + deviceId + ": " + device.isState());
+    }
+}
